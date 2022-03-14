95b55dcacfdf7414dd27dec702d1c746f9df4680
Burke Khoo <thootau99@tutanota.com>
2022-03-14 16:35:07 +0800

    [Changed] cache get

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.SERVER_PORT || "6666"
 
 const TEMP = "./temp"
 
+const tempGet = {}
+
 async function tempMonitoring() {
     const empty = await tempIsEmpty(TEMP)
     if (!empty) {
@@ -19,10 +21,37 @@ async function tempMonitoring() {
             } else {
                 await execQuery(_temp)
                 await cleanTemp(TEMP, `${_temp}\$#`)
-                console.log(await readTemp(TEMP))
             }
         })
     }
+}
+
+function getSelectResult(command) {
+    return new Promise(async (resolve, reject) => {
+        if (command in tempGet) {
+            // check result expired?
+            const saved = tempGet[command]
+            if (saved.date - new Date().getTime() > 10000) {
+                const result = await execQuery(command)
+                tempGet[command] = {
+                    value: result,
+                    date: new Date().getTime()
+                }
+                resolve(result)
+            } else {
+                resolve(saved.value)
+            }
+        } else {
+            if (ping(process.env.MYSQL_HOST)) {
+                const result = await execQuery(command)
+                tempGet[command] = {
+                    value: result,
+                    date: new Date().getTime()
+                }
+                resolve(result)
+            }
+        }
+    })
 }
 
 setInterval(() => {
@@ -30,8 +59,19 @@ setInterval(() => {
 }, 4000)
 
 app.use(express.json())
+
+app.post('/select', async (req, res) => {
+    try {
+        const command = req.body.command
+        const result = await getSelectResult(command)
+        res.send(result).status(200)
+    } catch (err) {
+        res.send(err).status(500)
+    }
+})
+
 app.post('/request', async (req, res) => {
-    const command = req.body.command || "test"
+    const command = req.body.command
     await writeTemp(TEMP, command)
     res.sendStatus(200)
 })
